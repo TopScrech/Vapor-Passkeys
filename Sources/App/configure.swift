1ab99958a99f9c49e6c08396fc698921e8e2510b
TopScrech <sergei_saliukov@icloud.com>
2024-08-16 14:52:05 +0200

    -

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -26,7 +26,7 @@ public func configure(_ app: Application) async throws {
     app.migrations.add(CreateWebauthnCredential())
     
     try await app.autoMigrate()
-    app.logger.logLevel = .error
+    app.logger.logLevel = .debug
     
     let domain = ""
     //    let domain = "bisquit-id.topscrech.dev"
